a45a4519a1bb3d5923f20feebab8838ef263c493
FrankLi123 <zoli@ucsd.edu>
2024-02-16 18:59:25 -0500

    copy code from inkathon repo for connection to Wallet

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 
+import { ConnectButton } from '@/src/components/web3/connect-button'
+
 export default function Home() {
   return (
+  <>
+    {/* Connect Wallet Button */}
+    <ConnectButton />
+
     <p>Hello, World!</p>
+    </>
   )
 }
