345370a361846cbd7b917e6323a3b371585d44a0
FrankLi123 <zoli@ucsd.edu>
2024-02-17 15:34:13 -0500

    Implemented Button Componenet for "filledPrimary" and "outlinedNeutral"

--- frontend/src/app/design/page.tsx
+++ frontend/src/app/design/page.tsx
@@ -1,10 +1,18 @@
 import {Rarity, RarityBadge} from "@/src/app/components/baseComponents/RarityBadge/RarityBadge";
 import {NftCard} from "@/src/app/components/baseComponents/NftCard/NftCard";
+import { FilledPrimaryButton } from "../components/baseComponents/Button/FilledPrimaryButton/FilledPrimaryButton";
+import { TextPrimaryButton } from "../components/baseComponents/Button/TextPrimaryButton/TextPrimaryButton";
+import { OutlinedNeutralButton } from "../components/baseComponents/Button/OutlinedNeutralButton/OutlinedNeutralButton";
+
+import {ButtonType, Button} from "@/src/app/components/baseComponents/Button/Button";
+import handleClickPrimaryButton from "./testFunction";
 
 export default function Design() {
     const badges = ["common", "uncommon", "rare", "epic", "legendary"].map((r) => {
         return <RarityBadge key={r} rarity={r as Rarity}/>
     });
+
+
     return (
         <>
             <div>
@@ -19,6 +27,13 @@ export default function Design() {
                     nftName={"Some NFT name"}
                     rarity={"legendary"}
                     price={"500,000"}/>
+            
+                    <h1>FilledPrimaryButton</h1> 
+                    <Button type={"primary"} cl="text_primary" ></Button>
+
+                    <h1>OutlinedNeutralButton</h1> 
+                    <Button type={"outlined"} cl="outlined" ></Button>
+
             </div>
         </>
     )
