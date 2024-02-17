3d46e746ba73b3c52bbf098c93fd41154ef6e2f5
FrankLi123 <zoli@ucsd.edu>
2024-02-17 15:34:13 -0500

    Implemented Button Componenet for "filledPrimary" and "outlinedNeutral"

---
+++ frontend/src/app/components/baseComponents/Button/Button.tsx
@@ -0,0 +1,13 @@
+import styles from './Button.module.css';
+// import {Rarity, RarityBadge} from "@/src/app/components/baseComponents/RarityBadge/RarityBadge";
+import Image from 'next/dist/shared/lib/image-external';
+export type ButtonType = "primary" | "outlined";
+
+export function Button({type, cl, onClick}: { type: ButtonType, cl?: string; onClick?: () => void  }) {
+
+    return (
+         <button className={`${styles.generalButton} bg-${type}-static hover:bg-${type}Hover click:bg-${type}Click`} onClick={onClick} >   
+            {cl}
+        </button>
+    )
+}
