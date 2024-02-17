0371cfb8ff03808530a498b6072fa245fdb55b07
FrankLi123 <zoli@ucsd.edu>
2024-02-17 18:52:29 -0500

    helper UI and Utils components from inkathon boilerplate for integration with wallet

--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { UploadForm } from "../components/uploadFile/UploadForm";
+import { ConnectButton } from "../components/web3/connect-button";
 
 export default function ServerUploadPage() {
   return (
     <main>
       <UploadForm />
+
+      <ConnectButton />
     </main>
   )
 }
