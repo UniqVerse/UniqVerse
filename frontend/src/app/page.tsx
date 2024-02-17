a493098b52f09656ccfbe0744ba8b77e7a9b0c38
FrankLi123 <zoli@ucsd.edu>
2024-02-17 17:02:50 -0500

    uncommented the line in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-// import { ValidateAndPublishNft } from "./components/validateAndPublishNft/ValidateAndPublishNft";
+import { ValidateAndPublishNft } from "./components/validateAndPublishNft/ValidateAndPublishNft";
 
 export default function Home() {
   return (
     <div>
-      {/* <ValidateAndPublishNft/> */}
+      <ValidateAndPublishNft/>
     </div>
   )
 }
