806b44a76e618c13c2bf093142468e6788160044
FrankLi123 <zoli@ucsd.edu>
2024-02-18 05:45:24 -0500

    fix merge error

--- frontend/src/app/components/uploadFile/UploadForm.tsx
+++ frontend/src/app/components/uploadFile/UploadForm.tsx
@@ -38,40 +38,67 @@ export function UploadForm({onSuccess}: { onSuccess?: (data: NftFileData) => voi
 
   return (
     
-  <form >
-    <div className="flex w-1/2 flex-col items-center justify-center pt-5 pb-6 border-2 border-dashed rounded-md">
-      
-      <label 
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" 
-        htmlFor="multiple_files"
-      >
-        Upload multiple files
-      </label>
-      <input 
-        className="block w-1/2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
-        id="multiple_files" 
-        type="file" 
-        name="file"
-        // ref={inputRef}
-        multiple 
-        // onChange={(e) => onChangeFile(e.target.files?.[0])}
-      />
-      <div className="flex flex-col items-center justify-center">
-        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-        </svg>
-        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or
-            drag and drop</p>
-        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX.
-            800x400px)</p>
-      </div>
 
-      { uploading && <p>Uploading...</p>}
-        <input type="submit" value="Upload" disabled={uploading} />
+    <>
+    <div>
+        {/*<form onSubmit={onSubmit}>*/}
+        {/*<input*/}
+        {/*    type="file"*/}
+        {/*    name="file"*/}
+        {/*    ref={inputRef}*/}
+        {/*    onChange={(e) => setFile(e.target.files?.[0])}*/}
+        {/*/>*/}
+        {/*<input type="submit" value="Upload" disabled={uploading}/>*/}
 
+        <div className={"text-center border-solid "}>
+            <div>
+                <button>Connect wallet</button>
+            </div>
+            <div className={''}>
+                {
+                uploading ? <Loading/> :
+                <UploadButton onChangeFile={onChangeFile}/>
+                }
+            </div>
+            <div className={"headline-large"}>Time to upload your amazing content 🤩</div>
+        </div>
     </div>
-    </form>
+</>
+
+  // <form >
+    
+  //   <div className="flex w-1/2 flex-col items-center justify-center pt-5 pb-6 border-2 border-dashed rounded-md">
+  //     <label 
+  //       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" 
+  //       htmlFor="multiple_files"
+  //     >
+  //       Upload multiple files
+  //     </label>
+  //     <input 
+  //       className="block w-1/2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+  //       id="multiple_files" 
+  //       type="file" 
+  //       name="file"
+  //       // ref={inputRef}
+  //       multiple 
+  //       // onChange={(e) => onChangeFile(e.target.files?.[0])}
+  //     />
+  //     <div className="flex flex-col items-center justify-center">
+  //       <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+  //             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+  //           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+  //                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+  //       </svg>
+  //       <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or
+  //           drag and drop</p>
+  //       <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX.
+  //           800x400px)</p>
+  //     </div>
+
+  //     { uploading && <p>Uploading...</p>}
+  //       <input type="submit" value="Upload" disabled={uploading} />
+
+  //   </div>
+  //   </form>
   )
 }
